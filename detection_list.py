c7b2ef09c185eb3c9736d5a3c0da066e5aca2253
Leonard Fabris <83458396+LFabris6@users.noreply.github.com>
2021-11-19 10:19:56 +0100

    Update detection_list.py

--- detection_list.py
+++ detection_list.py
@@ -1,10 +1,13 @@
 import jetson.inference
 import jetson.utils
+
 import time
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+
 from threading import Thread
+
 import tensorflow as tf
 from tensorflow import keras
 
@@ -13,73 +16,59 @@ path = '/home/robotika/jetson-inference/python/training/detection/ssd/models/tes
 
 
 net = jetson.inference.detectNet(argv=['--model={}'.format(path + "/ssd-mobilenet.onnx"), '--labels={}'.format(path + "/labels.txt"), '--input-blob=input_0', '--output-cvg=scores', '--output-bbox=boxes'], threshold=0.5)
-camera = jetson.utils.videoSource(1280, 720,"csi://0")
+camera = jetson.utils.videoSource("csi://0")
 
-'''
-zadaci:
-	važno:
-		1.Nisam testirao dio s klasifikacijom.
-	
-		2. crop slike daje exception 
-	
-	2.umijesto camera source od jetson utilsa koristiti opencv jer je sporo i radi probleme
-	Slike koje sada dobivam stalno treba pretvarati u numpy array
-	Ne može se baš tretirati njima kao array nego ima cijela dokumentacija za rad s njima
-	
-	
-	3. (manje bitno)Dataset za detekciju je napravljen od 40 slika lista na mom stolu, trebam jos slika uzet(isto sa stola da bude dobar dok ga testiram(kao da je stol vinograd), a u buducnosti
-	na razlicitim podrucjima pogotovo vinogradu.
-	
-	Kad važni zadaci budu rješeni, ovo će biti donekle funkcionalan detektor bolesti lišća, nadalje ga unaprijeđujemo.
-	Uz 2. i 3. će biti bolji.
-	
-		
-	
 
-'''
-model = tensorflow.keras.models.load_model("bolesti_vinoveloze.h5")
+model = tf.keras.models.load_model("bolesti.h5")
 
 klasifikacija = []
 spremne = []
+gotove =  []
 
 class Procesiranje(Thread):
     def __init__(self):
+	
         Thread.__init__(self)
         self.daemon = True
         self.start()
 
     def run(self):
-    	while True:
-    		if(len(klasifikacija)):
+    	
+        while True:
+            if(len(klasifikacija)):
 
-    			for detection, img in klasifikacija:
-    				try:
+                for detection, img in klasifikacija:
+                    try:
+                        img_arry = jetson.utils.cudaToNumpy(img)
+                        img_arry = cv2.resize(img_arry, (256, 256))
+                        klasifikacija.pop(klasifikacija.index([detection, img]))
+                        spremne.append(img_arry)
 
-    					img_cropped = jetson.utils.cudaAllocMapped(width=detection.Width, height=detection.Height, format=img.format)
-                			crop_roi=(detection.Left, detection.Top, detection.Right, detection.Bottom)
-                			jetson.utils.cudaCrop(img, img_cropped, crop_roi)
-
-                			img_cropped = jetson.utils.cudaToNumpy(img_cropped)
-                			img_cropped = img_cropped/255.0
-                			#img_cropped = img_cropped.reshape(-1, 256, 256, 3)
-                			spremne.append(img_cropped)
-
-                			klasifikacija.pop(klasifikacija.index([detection, img]))
+                        
 
 
+                    except Exception as e:
+                        print(e)
+
+                try:
+                    druge = np.array(spremne).reshape(-1, 256, 256, 3)
+                    prediction = model.predict(druge)
+
+                    for i in range(len(prediction)):
+                        b = np.argmax(prediction[i])
+                        cv2.imshow(str(b), druge[i])
+                        cv2.waitKey(0)
+
+                        gotove.append([b, druge[i]])
+                        spremne.clear()
+
+                        
+
+                except Exception as e:
+                    print(e)
 
 
-                		except Exception as e:
-                			print(e)
-               			 try:
-                			spremne = np.array(spremne).reshape(-1, 256, 256, 3)
-                			model.predict(spremne)
-
-                		except:
-                			print("greška prilikom klasifikacije")
-
-
-    				
+                    
 
 
             
@@ -96,7 +85,7 @@ class Detekcija(Thread):
         
         while True:
 
-            img, width, height = camera.Capture()
+            img = camera.Capture()
             detections = net.Detect(img, overlay='none')
   
 
